0486b3c29bed30774a53d8ee4d439f3f57ee9e9f
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2017-04-30 04:31:11 +0900

    logging

--- bot.go
+++ bot.go
@@ -3,15 +3,21 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	ulai "github.com/m0t0k1ch1/go-ulai"
 	mastodon "github.com/mattn/go-mastodon"
+)
+
+const (
+	LogPrefix = "[mastodon-pug] "
 )
 
 type Bot struct {
 	config Config
 	ai     *ulai.Client
 	mstdn  *mastodon.Client
+	logger *log.Logger
 }
 
 func NewBot(config Config) *Bot {
@@ -22,7 +28,16 @@ func NewBot(config Config) *Bot {
 		config: config,
 		ai:     ai,
 		mstdn:  mastodon.NewClient(config.Mastodon),
+		logger: log.New(
+			os.Stdout,
+			LogPrefix,
+			log.Ldate|log.Ltime,
+		),
 	}
+}
+
+func (bot *Bot) log(v ...interface{}) {
+	bot.logger.Println(v)
 }
 
 func (bot *Bot) Run() error {
@@ -30,6 +45,8 @@ func (bot *Bot) Run() error {
 	if err != nil {
 		return err
 	}
+
+	bot.log("start streaming")
 
 	for ev := range evChan {
 		switch evType := ev.(type) {
